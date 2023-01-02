1d7b5fb7fb23e0d87fd03304bb4654abf83d8532
mufis-coder <muhamadfikrisunandar@gmail.com>
2023-01-02 12:11:47 +0700

    feat: add chat gpt3 for generating answer

--- chatbot.py
+++ chatbot.py
@@ -1,13 +1,19 @@
+import os
 import random
 import json
 import pickle
 import numpy as np
+from dotenv import load_dotenv
 
 import nltk
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 
 from tensorflow.keras.models import load_model
+
+import openai
+
+load_dotenv()
 
 ERROR_THRESHOLD = 0.25
 
@@ -38,13 +44,14 @@ def bag_of_words(sentence):
 def predict_class(sentence):
     bow = bag_of_words(sentence)
     res = model.predict(np.array([bow]))[0]
-    results = [[i, r] for i,r in enumerate(res) if r > ERROR_THRESHOLD]
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
 
     results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
     for r in results:
         return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
     return return_list
+
 
 def get_response(intents_list, intents_json):
     tag = intents_list[0]['intent']
@@ -55,11 +62,31 @@ def get_response(intents_list, intents_json):
             break
     return result
 
+
+def answer_generator(quest, ans):
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+
+    prompt_gpt = f'parafrase kalimat berikut dengan gaya kasual, seperti sedang menjawab pertanyaan "{quest}?": "{ans}"'
+
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt_gpt,
+        temperature=0.8,
+        max_tokens=709,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    res_text = response.choices[0].text
+    res = res_text[res_text.rindex('\n')+1:]
+    return res
+
+
 print("FisBot is running!")
 
 while True:
     message = input("")
     ints = predict_class(message)
     res = get_response(ints, intents)
-    print(res)
-    print()
+    res = answer_generator(message, res)
+    print("FisBot: " + res)
